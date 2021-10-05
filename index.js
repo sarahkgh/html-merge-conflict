87b78a21984df73ba9d7f29ae3feb48c3aa0e6d9
reece-elder <reece.elder@qa.com>
2021-10-05 11:55:02 +0100

    added html and js

---
+++ index.js
@@ -0,0 +1,27 @@
+`use strict`;
+
+let nameList = [
+    "reece",
+    "Tony"
+];
+
+let foods = [
+    "pizza", 
+    "Lasagna"
+];
+
+const randName = nameList[Math.floor(Math.random() * nameList.length)];
+const randFood = foods[Math.floor(Math.random() * foods.length)];
+
+let websiteString = document.createElement('h2');
+websiteString.textContent = `${randName}'s favourite food is ${randFood}`;
+
+let websiteTitle = document.querySelector('#title');
+console.log(websiteTitle);
+console.log(websiteString);
+
+websiteTitle.appendChild(websiteString);
+
+
+console.log(randName);
+console.log(randFood);

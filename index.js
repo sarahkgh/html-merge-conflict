5edde2a9c05edf50d1942765f9c248d862cd4a9a
sarahkgh <91898546+sarahkgh@users.noreply.github.com>
2021-10-05 12:51:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 
 let nameList = [
     "Reece",
-    "Tony"
-];
-
+    "Tony",
+    "Sarah"
+    ];
 let foods = [
     "Pizza", 
-    "Lasagna"
+    "Lasagna",
+    "Pasta"
 ];
 
 const randName = nameList[Math.floor(Math.random() * nameList.length)];
